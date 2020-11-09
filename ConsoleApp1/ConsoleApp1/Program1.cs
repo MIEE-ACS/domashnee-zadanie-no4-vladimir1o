5f4560ed8c24a786c3f4c220bd7d0cbbae592acf
vladimir1o <54781268+vladimir1o@users.noreply.github.com>
2020-11-09 22:55:34 +0300

    Сделал вариант 7

---
+++ ConsoleApp1/ConsoleApp1/Program1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+
+namespace ConsoleApp1
+{
+    class Program1
+    {
+        static void Main()
+        {
+            Console.Write("Введите количество чисел: ");
+            int N;
+            Random rnd = new Random();
+
+            try
+            {
+                N = int.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Wrong data!");
+                Console.Write("\nВведите количество чисел (числом): ");
+                N = int.Parse(Console.ReadLine());
+                Console.ReadKey();
+            }
+
+
+            if (N <= 0)
+            {
+                Console.WriteLine("Введены неправельные данные!");
+                Console.ReadKey();
+            }
+            else
+            {
+                int[] nums = new int[N];
+                int max = nums[0];
+                int maxNum = 0;
+                bool zeroinNums = false;
+
+                //Заполняем массив и ищем максимальный элемент
+                for (int i = 0; i < N; i++)
+                {
+                    nums[i] = rnd.Next(0, 10);
+                    Console.Write($"{nums[i]} ");
+                    if (nums[i] > max)
+                    {
+                        max = nums[i];
+                        maxNum = i;
+                    }
+                    if (nums[i] == 0) {zeroinNums = true;}
+                }
+
+                Console.WriteLine($"\n\nМаксимальный элемент: {max}, находится на {maxNum + 1} позиции\n");
+
+                // Считаем произведения элементов между нулями (либо между нулём и концом массива)
+                if (zeroinNums)
+                {
+                    int firstZero = Array.IndexOf(nums, 0);
+                    int i = firstZero;
+                    int multiplication = 1;
+
+                    if ((firstZero == N - 1) || (firstZero == N - 2 && (N - 1 == 0))) { multiplication = 0;}
+                    else
+                    {
+                        while (i != N - 1)
+                        {
+                            i++;
+                            if (nums[i] == 0) { break; }
+                            multiplication = nums[i] * multiplication;
+                        }
+                    }                 
+                    Console.WriteLine($"Произведение элементов массива, расположенных между первым и вторым нулевыми элементами: {multiplication}\n");
+                }
+                else { Console.WriteLine("Нулей в массиве нет!\n"); }
+
+                //Сортируем массив
+                //пытался всё сделать в 2 for, но не вышло тк для нечётных N 
+                //нужно разное количество итераций для чётных и нечётных элементов
+
+                Console.WriteLine("Отсортированный массив");
+
+                int[] nums2 = new int[N];
+                int med;
+                if ((N % 2) == 0) { med = (N / 2); }
+                else { med = ((N - 1) / 2) + 1;}
+
+                for (int m = 0, j = 0; j < med; m += 2, j++) { nums2[j] = nums[m]; }
+                
+                for (int j = 0, k = 1; (med + j) < N; j++, k += 2) { nums2[med + j] = nums[k]; }
+
+                for (int j = 0; j < N; j++) { Console.Write($"{nums2[j]} "); }
+
+                Console.ReadKey();
+            }
+                
+            
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp2/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Как заполним двумерный массив (8x8)?\n0-рандомно\n1-вручную");
+            int choise;
+            int[][] arr = new int[8][];
+            Random rnd = new Random();
+
+            try
+            {
+                choise = int.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Wrong data!");
+                Console.WriteLine("\n0-рандомно\n1-вручную\n");
+                choise = int.Parse(Console.ReadLine());
+                Console.ReadKey();
+            }
+
+            if (choise == 0)
+            {
+                for (int i = 0; i < 8; i++)
+                {
+                    int[] newarr = new int[8];
+                    for (int j = 0; j < 8; j++)
+                    {
+                        newarr[j] = rnd.Next(-10, 10);
+                    }
+                    arr[i] = newarr;
+                }
+            }
+
+            else if (choise == 1)
+            {
+                
+                Console.WriteLine("Пример ввода значений: 1 2 3 4 5 6 7 8\nВ массиве первый элемент строки будет равен 1, второй 2 и т.д.");
+                try
+                {
+                    for (int i = 0; i < 8; i++)
+                    {
+                        Console.WriteLine($"Введите 8 элементов {i + 1} строки (через пробел!) ");
+                        arr[i] = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Проблема при вводе значений, перезапустите программу и попробуйте снова!");
+                    Console.ReadKey();
+                }
+                
+            }
+
+            try
+            {
+                foreach (int[] row in arr)
+                {
+                    foreach (int number in row)
+                    {
+                        Console.Write($"{number} \t");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Проблема при вводе значений, перезапустите программу и попробуйте снова!");
+                Console.ReadKey();
+            }
+
+            try
+            {
+                for (int i = 0, l = 0; i < arr.Length; i++, l++)
+                {
+                    int match = 0;
+                    for (int j = 0, m = 0; j < arr[i].Length; j++, m++)
+                    {
+                        if (arr[m][l] == arr[i][j]) { match++; }
+                        if (match == 8) { Console.Write($"\n{i + 1} строка и {l + 1} столбец совпадают"); }
+                    }
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Проблема при поиске совпадений, это может говорить о том что в какой-то строке не 8 элементов." +
+                    "\nПерезапустите программу и попробуйте снова!");
+                Console.ReadKey();
+            }
+
+            bool isnegative;
+            int sum;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                isnegative = false;
+                sum = 0;
+
+                for (int j = 0; j < arr[i].Length; j++)
+                {
+                    if (arr[i][j] < 0) 
+                    { 
+                        isnegative = true;
+                        sum = arr[i].Sum();
+                    }        
+                }
+
+                if (isnegative) { Console.WriteLine($"\nСумма элементов {i + 1} строки: {sum}"); }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
